b25c2031285899d65f09c5ca9ed795c473b91b8c
Nicholas Stevens <nstevens@nanick.org>
2021-06-22 00:37:02 -0500

    Cleaned up solution directory.

--- Twitter.ApiCaller/Properties/AssemblyInfo.cs
+++ Twitter.ApiCaller/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Twitter.ApiCaller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET Framework library used to interact with Twitter API v2 and v1.1")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("nstevens1040")]
 [assembly: AssemblyProduct("Twitter.ApiCaller")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
